c5adc58c58515b88da585d161f9e1a2d2cafe88e
Marwan Hisham <marwanhiisham@gmail.com>
2025-06-08 20:21:36 +0300

    (feat): make it work

--- apps/app/app/(authenticated)/page.tsx
+++ apps/app/app/(authenticated)/page.tsx
@@ -1,21 +1,10 @@
-import { env } from '@/env';
 import { auth } from '@repo/auth/server';
 
 import type { Metadata } from 'next';
-import dynamic from 'next/dynamic';
 import { notFound } from 'next/navigation';
-import { AvatarStack } from './components/avatar-stack';
-import { Cursors } from './components/cursors';
-import { Header } from './components/header';
 
 const title = 'Acme Inc';
 const description = 'My application.';
-
-const CollaborationProvider = dynamic(() =>
-  import('./components/collaboration-provider').then(
-    (mod) => mod.CollaborationProvider
-  )
-);
 
 export const metadata: Metadata = {
   title,
@@ -30,19 +19,9 @@ const App = async () => {
   }
 
   return (
-    <>
-      <Header pages={['Building Your Application']} page="Data Fetching">
-        {env.LIVEBLOCKS_SECRET && (
-          <CollaborationProvider orgId={orgId}>
-            <AvatarStack />
-            <Cursors />
-          </CollaborationProvider>
-        )}
-      </Header>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
-    </>
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    </div>
   );
 };
 
